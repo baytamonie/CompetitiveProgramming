15f68ae909978462735251a12a01f12a3fc95711
Mohammad <mbayta@outlook.com>
2021-01-01 22:17:03 +0200

    finished cummualtive usm

--- CummalitiveSum/IlyaandQueries.cpp
+++ CummalitiveSum/IlyaandQueries.cpp
@@ -1,26 +1,27 @@
 #include<iostream>
-
 using namespace std;
+
 string s;
 int c;
-int n,m;
+int m;
 int l,r;
+int cs[100000]={0};
+int j=1;
 int main(){
 
-    cin>>s;
-    cin>>m;
-    for(int i=0;i<m;i++){
-
-        cin>>l>>r; 
-        c=0;
-        for(int j=l ;j <r-1;j++){
-
-            if (s[j]==s[j+1])
-                c++;
-            
-        }
-        cout<<c<<"\n";
-
+    cin>>s>>m;
+    for(int i =0;i<s.length()-1;i++){
+        if(s[i]==s[i+1])
+            cs[j]=cs[j-1]+1;
+        else 
+            cs[j]=cs[j-1];
+        j++;
     }
 
+    for(int i =0;i<m;i++){
+        cin>>l>>r;
+        cout<<cs[r-1]-cs[l-1]<<"\n";
+    }
+
+    
 }
